8e1e74ad147380bb0e7b6140b6d391af71d43245
LucasKernan <LucasJKernan@proton.me>
2024-06-05 14:11:17 +0900

    Update tiff_3d_viewer.py

--- tiff_3d_viewer.py
+++ tiff_3d_viewer.py
@@ -2,6 +2,7 @@ import numpy as np
 from PIL import Image
 import os
 import matplotlib.pyplot as plt
+import cv2
 
 def load_tiff_stack(directory):
     """Load a stack of TIFF images from a directory."""
@@ -12,9 +13,16 @@ def load_tiff_stack(directory):
             tiff_stack.append(np.array(img))
     return np.array(tiff_stack)
 
+def enhance_contrast(image):
+    """Enhance the contrast of the image."""
+    if len(image.shape) == 3 and image.shape[2] == 3:
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+    return cv2.equalizeHist(image)
+
 def visualize_slice(stack, index):
     """Visualize a specific slice from the 3D stack."""
-    plt.imshow(stack[index], cmap='gray')
+    enhanced_slice = enhance_contrast(stack[index])
+    plt.imshow(enhanced_slice, cmap='gray')
     plt.title(f'Slice {index}')
     plt.show()
 
@@ -25,3 +33,13 @@ tiff_stack = load_tiff_stack(tiff_directory)
 # Visualize the middle slice
 middle_slice = len(tiff_stack) // 2
 visualize_slice(tiff_stack, middle_slice)
+
+# Skeleton for unwrapping (to be implemented)
+def unwrap_papyrus(stack):
+    """Unwrap the papyrus using a basic approach (to be improved)."""
+    # Placeholder for unwrapping algorithm
+    # This is a complex problem that would need advanced techniques
+    pass
+
+# Example usage of the unwrapping function (currently does nothing)
+unwrap_papyrus(tiff_stack)
